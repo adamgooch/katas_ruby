235e9fd3ff68c53fba9bbd75878c6f796d9309a0
Adam Gooch <adamgooch@acm.org>
2012-06-03 03:50:14 -0400

    Bowling kata passes spares test. (as long as the spare isn't in the 10th frame)

--- bowling.rb
+++ bowling.rb
@@ -1,8 +1,18 @@
 class Bowling
    def get_score frames
       score = 0
+      frameScore = 0
+      roll = 0
+      multiplier = 1
       frames.split("").each do |i|
-         score += i.to_i
+         rollScore = i.to_i
+         roll += 1
+         score += (rollScore * multiplier)
+         if roll % 2 == 1 #first roll of frame 
+            frameScore = 0
+         end
+         frameScore += rollScore
+         frameScore == 10 ? multiplier = 2 : multiplier = 1
       end
       score
    end
--- specs/bowling_spec.rb
+++ specs/bowling_spec.rb
@@ -13,9 +13,27 @@ describe Bowling do
    end
 
    describe "get_score" do
+      # no marks
       it "returns 63 when given the frames: '31415390107133238009'" do
          @bowling.get_score("31415390107133238009").should == 63
       end
+
+      # another test with no marks
+      it "returns 68 when given the frames: '30417281904523135253'" do
+         @bowling.get_score("30417281904523135253").should == 68
+      end
+
+      # first frame has a spare
+      it "returns 73 when given the frames: '91415390107133238009'" do
+         @bowling.get_score("91415390107133238009").should == 73
+      end
+
+      # multiple spares
+      it "returns 101 when given the frames: '14824272735362645543'" do
+         @bowling.get_score("14824272735362645543").should == 101
+      end
+
+
    end
 
 end
